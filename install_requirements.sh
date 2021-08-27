6d28176391594858150f5999749fdbff57a3d504
abhi-stylumia <85272346+abhi-stylumia@users.noreply.github.com>
2021-08-27 12:19:09 +0530

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-apt-get update && apt-get install $INPUT_OS_DEPENDENCIES -y
+apt-get update
+apt-get install $INPUT_OS_DEPENDENCIES -y
 
 echo $(find . -name requirements.txt)
+echo $GITHUB_WORKSPACE
 
 pip install -r requirements.txt
